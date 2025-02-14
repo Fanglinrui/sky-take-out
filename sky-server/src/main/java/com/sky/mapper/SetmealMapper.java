66fae76a06f5e5c504b9c2485f6b4bc29e20893d
Fanglinrui <Fanglinrui@bupt.edu.cn>
2025-02-14 22:04:46 +0800

    修改套餐业务代码

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -49,4 +49,11 @@ public interface SetmealMapper {
      * @param ids
      */
     void deletByIds(List<Long> ids);
+
+    /**
+     * 更新套餐
+     * @param setmeal
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Setmeal setmeal);
 }
