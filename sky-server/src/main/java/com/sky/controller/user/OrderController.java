d4b8a80a8c6c187f36b1151f67a0892fe510ca37
Fanglinrui <Fanglinrui@bupt.edu.cn>
2025-02-18 09:42:41 +0800

    历史订单查询业务代码

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -2,6 +2,7 @@ package com.sky.controller.user;
 
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
@@ -48,7 +49,22 @@ public class OrderController {
         log.info("订单支付：{}", ordersPaymentDTO);
          orderService.payment(ordersPaymentDTO);
 //        log.info("生成预支付交易单：{}", orderPaymentVO);
-        log.info("跳过一切，直接成功");
+        log.info("跳过微信支付逻辑，直接成功");
         return Result.success();
+    }
+
+    /**
+     * 历史订单查询
+     *
+     * @param page
+     * @param pageSize
+     * @param status   订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消
+     * @return
+     */
+    @GetMapping("/historyOrders")
+    @ApiOperation("历史订单查询")
+    public Result<PageResult> page(int page, int pageSize, Integer status) {
+        PageResult pageResult = orderService.pageQuery4User(page, pageSize, status);
+        return Result.success(pageResult);
     }
 }
